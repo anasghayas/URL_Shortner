3c3eff4c7be22e00ba6eef39ac1168854ee2ef50
anasghayas <anasghayas2004@gmail.com>
2026-03-07 01:32:07 +0530

    Backend_Done

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express=require("express");
 const urlRoute=require("./routes/url");
 const {connectToMongoDb}=require("./connect");
+const URL=require("./models/url");
 const PORT=8001;
 const app=express();
 
 connectToMongoDb("mongodb://localhost:27017/url-Shortner").then(()=>console.log("MongoDb Connected"));
 
 app.use(express.json());
-app.use('/url',urlRoute);
+app.use("/url",urlRoute);
+
+app.get("/:shortId",async(req,res)=>{
+    const shortId=req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {shortId},
+        {$push:{
+            visitHistory : {
+                timestamp:Date.now(),
+            },
+        },
+    },
+    );
+    res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT,()=>console.log(`SERVER STARTED at PORT : ${PORT}`));
